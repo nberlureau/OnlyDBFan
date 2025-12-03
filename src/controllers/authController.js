d886a457aba66135fc0ad3170d94dc32132eb9e0
Alex Lajoinie <alex.lajoinie@etu.unilim.fr>
2025-12-03 17:48:33 +0100

    [FIX] correction pour swagger

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,25 +3,25 @@ const User = require('../models/User');
 /**
  * @swagger
  * /auth/register:
- * post:
- * summary: Inscrire un nouvel utilisateur
- * tags: [Auth]
- * requestBody:
- * required: true
- * content:
- * application/json:
- * schema:
- * type: object
- * properties:
- * username:
- * type: string
- * email:
- * type: string
- * password:
- * type: string
- * responses:
- * 201:
- * description: Utilisateur créé
+ *   post:
+ *     summary: Inscrire un nouvel utilisateur
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Utilisateur créé
  */
 exports.register = async (req, res) => {
     try {
@@ -32,6 +32,52 @@ exports.register = async (req, res) => {
         await newUser.save();
 
         res.status(201).json({ message: "Utilisateur créé !", userId: newUser._id });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Connecter un utilisateur
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Connexion réussie
+ */
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: "Utilisateur non trouvé" });
+        }
+
+        // Note: En production, il faudrait vérifier le mot de passe hashé
+        if (user.password !== password) {
+            return res.status(401).json({ message: "Mot de passe incorrect" });
+        }
+
+        res.status(200).json({
+            message: "Connexion réussie",
+            userId: user._id,
+            username: user.username
+        });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
